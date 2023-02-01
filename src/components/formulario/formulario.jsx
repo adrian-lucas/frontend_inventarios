adfeea5c2925d58f4f124001e1cdf96ccf3e5708
adrian-lucas <donplucas@gmail.com>
2023-02-01 08:11:05 -0400

    control de inputs realizado

--- src/components/formulario/formulario.jsx
+++ src/components/formulario/formulario.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect,useRef } from "react";
 
 function Formulario(){
  const [firstName, setFirstName] = useState('');
@@ -6,27 +6,69 @@ function Formulario(){
  const [email, setEmail] = useState('');
  const [userName,setUserName] = useState('');
  const [gender,setGender] = useState('');
- const verFirstName = (e)=>{
-    setFirstName(e.target.value);
-    console.log(firstName);
- }
- const mostrarDatos = (e)=>{
-    alert(`Nombre: ${firstName} ${secondName} \n Email: ${email} \n Username: ${userName} \n Gender: ${gender}`);
+ const [errores,setErrores] = useState({firstName:false,secondName:false});
+ const disabled = useRef(null); 
+
+ const verDatos = (e)=>{
     e.preventDefault();
+    let resError = 'ERROR valores inválidos';
+    let resExito = `Nombre: ${firstName} ${secondName} \n Email: ${email} \n Username: ${userName} \n Gender: ${gender}`;
+    let valido = true;
+    if(firstName === ''){
+        resError+=`\n First Name :${firstName}`;
+        valido = false;
+    }
+    if(secondName === ''){
+        resError+= `\n Second Name : ${secondName};`;
+        valido = false;
+    }
+    if(userName === ''){
+        resError+= `\n User Name: ${userName};`;
+        valido = false;
+    }
+    if(gender === ''){
+        resError+= `\n Gender: ${gender}`;
+        valido = false;
+    }
+
+    alert(valido?resExito:resError);
+ }
+
+
+ const verNames = (name)=>{
+    let res = true;
+    let cont = 0; 
+    while(res && cont < name.length){
+        res = name.charAt(cont) <= 'z' && name.charAt(cont) >= 'a'
+        cont++;
+    }
+    if(disabled.current){
+     disabled.current.disabled = !res;
+     console.log(disabled.current);
+    }
+
+    return res;
  }
 
 
 
     return (
         <div className = "w3-margin w3-border" style = {{maxWidth:'600px'}}>
-            <form onSubmit = {mostrarDatos} className="w3-container">
+            <form onSubmit = {verDatos} className="w3-container">
                 <label  className="w3-container">
                     First Name: 
                     <input 
                         className = "w3-right" 
                         type = "text" 
                         value = {firstName} 
-                        onChange = {(e)=>setFirstName(e.target.value)} 
+                        onChange = {
+                            (e)=>{
+                            setFirstName(e.target.value); 
+                            setErrores(verNames(e.target.value)?{...errores,firstName:false}:{...errores,firstName:true});
+                            }
+                        }
+                        style = {errores.firstName?{color:'red'}:{color:'green'}} 
+                        
                     />
                 </label>
                 <br />
@@ -36,7 +78,14 @@ function Formulario(){
                         className = "w3-right" 
                         type = "text" 
                         value = {secondName}
-                        onChange = {(e)=>setSecondName(e.target.value)}
+                        onChange = {
+                            (e)=>{
+                                setSecondName(e.target.value); 
+                                setErrores(verNames(e.target.value)?{...errores,secondName:false}:{...errores,secondName:true});
+                            }
+                        }
+                        style = {errores.secondName?{color:'red'}:{color:'green'}}
+                        
                     />
                 </label>
                 <br />
@@ -44,7 +93,7 @@ function Formulario(){
                     Email: 
                     <input 
                         className = "w3-right" 
-                        type = "text"
+                        type = "email"
                         value = {email}
                         onChange = {(e)=>setEmail(e.target.value)} 
                     />
@@ -76,7 +125,9 @@ function Formulario(){
                             <option value="other">Other</option>
                     </select>
                 </label>
-                <input type="submit" value="Mostrar"/>
+                <label className="w3-container">
+                    <input className="w3-container" type="submit" value = "Enviar"  ref = {disabled}/>
+                </label>
             </form>
         </div>
     );
